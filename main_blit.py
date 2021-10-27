27d51052694e61f2bd1c311767922e31e1be2cd2
Elias Olofsson <65086086+eliaserland@users.noreply.github.com>
2021-10-27 11:09:55 +0200

    Fixed indentation.

--- main_blit.py
+++ main_blit.py
@@ -13,6 +13,8 @@ from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds, Brai
 from brainflow.data_filter import DataFilter, FilterTypes, AggOperations, NoiseTypes, WindowFunctions, DetrendOperations
 from brainflow.ml_model import BrainFlowMetrics, BrainFlowClassifiers, BrainFlowModelParams, MLModel
 
+import matplotlib
+#matplotlib.use('GTK3Agg')
 from matplotlib import pyplot as plt
 from matplotlib.gridspec import GridSpec
 logging.getLogger('matplotlib').disabled = True
@@ -23,76 +25,76 @@ fps = -1
 lastTime = time.time()
 
 class BlitManager:
-    def __init__(self, canvas, animated_artists=()):
-        """
-        Parameters
-        ----------
-        canvas : FigureCanvasAgg
-            The canvas to work with, this only works for sub-classes of the Agg
-            canvas which have the `~FigureCanvasAgg.copy_from_bbox` and
-            `~FigureCanvasAgg.restore_region` methods.
+	def __init__(self, canvas, animated_artists=()):
+		"""
+		Parameters
+		----------
+		canvas : FigureCanvasAgg
+		The canvas to work with, this only works for sub-classes of the Agg
+		canvas which have the `~FigureCanvasAgg.copy_from_bbox` and
+		`~FigureCanvasAgg.restore_region` methods.
 
-        animated_artists : Iterable[Artist]
-            List of the artists to manage
-        """
-        self.canvas = canvas
-        self._bg = None
-        self._artists = []
+		animated_artists : Iterable[Artist]
+		List of the artists to manage
+		"""
+		self.canvas = canvas
+		self._bg = None
+		self._artists = []
 
-        for a in animated_artists:
-            self.add_artist(a)
-        # grab the background on every draw
-        self.cid = canvas.mpl_connect("draw_event", self.on_draw)
+		for a in animated_artists:
+			self.add_artist(a)
+		# grab the background on every draw
+		self.cid = canvas.mpl_connect("draw_event", self.on_draw)
 
-    def on_draw(self, event):
-        """Callback to register with 'draw_event'."""
-        cv = self.canvas
-        if event is not None:
-            if event.canvas != cv:
-                raise RuntimeError
-        self._bg = cv.copy_from_bbox(cv.figure.bbox)
-        self._draw_animated()
+	def on_draw(self, event):
+		"""Callback to register with 'draw_event'."""
+		cv = self.canvas
+		if event is not None:
+			if event.canvas != cv:
+				raise RuntimeError
+		self._bg = cv.copy_from_bbox(cv.figure.bbox)
+		self._draw_animated()
 
-    def add_artist(self, art):
-        """
-        Add an artist to be managed.
+	def add_artist(self, art):
+		"""
+		Add an artist to be managed.
 
-        Parameters
-        ----------
-        art : Artist
+		Parameters
+		----------
+		art : Artist
 
-            The artist to be added.  Will be set to 'animated' (just
-            to be safe).  *art* must be in the figure associated with
-            the canvas this class is managing.
+		The artist to be added.  Will be set to 'animated' (just
+		to be safe).  *art* must be in the figure associated with
+		the canvas this class is managing.
 
-        """
-        if art.figure != self.canvas.figure:
-            raise RuntimeError
-        art.set_animated(True)
-        self._artists.append(art)
+		"""
+		if art.figure != self.canvas.figure:
+			raise RuntimeError
+		art.set_animated(True)
+		self._artists.append(art)
 
-    def _draw_animated(self):
-        """Draw all of the animated artists."""
-        fig = self.canvas.figure
-        for a in self._artists:
-            fig.draw_artist(a)
+	def _draw_animated(self):
+		"""Draw all of the animated artists."""
+		fig = self.canvas.figure
+		for a in self._artists:
+			fig.draw_artist(a)
 
-    def update(self):
-        """Update the screen with animated artists."""
-        cv = self.canvas
-        fig = cv.figure
-        # paranoia in case we missed the draw event,
-        if self._bg is None:
-            self.on_draw(None)
-        else:
-            # restore the background
-            cv.restore_region(self._bg)
-            # draw all of the animated artists
-            self._draw_animated()
-            # update the GUI state
-            cv.blit(fig.bbox)
-        # let the GUI event loop process anything it has to do
-        cv.flush_events()
+	def update(self):
+		"""Update the screen with animated artists."""
+		cv = self.canvas
+		fig = cv.figure
+		# paranoia in case we missed the draw event,
+		if self._bg is None:
+			self.on_draw(None)
+		else:
+			# restore the background
+			cv.restore_region(self._bg)
+			# draw all of the animated artists
+			self._draw_animated()
+			# update the GUI state
+			cv.blit(fig.bbox)
+		# let the GUI event loop process anything it has to do
+		cv.flush_events()
 
 class Graph:
 	def __init__(self, board_shim):
@@ -219,10 +221,9 @@ class Graph:
 			# Notch filter, remove 50Hz AC interference.
 			DataFilter.remove_environmental_noise(data[channel], self.sampling_rate, NoiseTypes.FIFTY)
 
-
-			#DataFilter.detrend(data[channel], DetrendOperations.CONSTANT.value)
-			#DataFilter.perform_bandpass(data[channel], self.sampling_rate, 51.0, 100.0, 2,
-			#						FilterTypes.BUTTERWORTH.value, 0)
+			DataFilter.detrend(data[channel], DetrendOperations.CONSTANT.value)
+			DataFilter.perform_bandpass(data[channel], self.sampling_rate, 51.0, 100.0, 2,
+									FilterTypes.BUTTERWORTH.value, 0)
 			#DataFilter.perform_bandpass(data[channel], self.sampling_rate, 51.0, 100.0, 2,
 			#							FilterTypes.BUTTERWORTH.value, 0)
 			#DataFilter.perform_bandstop(data[channel], self.sampling_rate, 50.0, 4.0, 2,
